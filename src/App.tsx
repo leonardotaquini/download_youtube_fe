96a3e317f741da69cb649bb9c6cada6b00e8778b
Leonardo Taquini <leonardogtaquini@gmail.com>
2024-04-18 14:32:25 -0300

    app supuestamente lista

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,23 @@ function App() {
 
   const [url, setUrl] = useState('');
   const [downloadUrl, setDownloadUrl] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+ 
+  
 
   const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
     try {
         setDownloadUrl('');
-        const { data } = await axios.post('http://localhost:8000/download/', {
+        const { data } = await axios.post('https://download-youtube-o9lv.onrender.com/download/', {
           url
         } );
-        setDownloadUrl(data.url_download);      
+        setDownloadUrl(data.url_download); 
+        setIsLoading(false);     
     } catch (error) {
+      setIsLoading(false);
       console.log(error)
     }
     
@@ -24,6 +31,7 @@ function App() {
   }
 
   const download = () => {
+  
     const a = document.createElement('a');
     a.href = downloadUrl;
     a.click();
@@ -32,25 +40,36 @@ function App() {
   }
 
   return (
-    <div className='vh-100 d-flex flex-column'>
-      <h1 className='d-flex justify-content-center'>
+    <div className='vh-100 d-flex flex-column justify-content-center align-items-center mt-5'>
+      <h1 className='d-flex justify-content-center shadow p-3 rounded my-5'>
         <p className='h1' style={{color: 'red'}}>Youtube</p> 
         <p className='h1 text-secondary'>Downloader</p>
-        </h1>
-      <div className=' m-auto col-6'>
+      </h1>
+      
+        
+      <div className='col-11 col-sm-10 col-lg-6 p-2'>
         <h5 className='text-center'>Ingresa el link</h5>
-        <form className='d-flex flex-column flex-sm-row flex-lg-row' onSubmit={ handleSubmit }>
-          <input type="text" className='form-control' value={ url } onChange={(e) => setUrl(e.target.value)}/>
-          <button type="submit"className='btn btn-outline-primary col-12 col-sm-4 col-lg-3'  >Buscar</button>
+        <form className='d-flex flex-column flex-sm-row flex-lg-row ' onSubmit={ handleSubmit }>
+          <input type="text" className='form-control m-2' value={ url } onChange={(e) => setUrl(e.target.value)}/>
+          <button type="submit"className='btn btn-outline-primary col-12 col-sm-4 col-lg-3 m-2'  >Buscar</button>
         </form>
       </div>
-      <div>
+      <div className='my-5'>
         {
           downloadUrl && 
           <div>
             <button className='btn btn-primary' onClick={ download }>Descargar</button>
+
           </div>
           
+        }
+        {
+          isLoading &&
+            <div className="text-center">
+              <div className="spinner-border" role="status">
+                <span className="visually-hidden">Loading...</span>
+              </div>
+            </div>
         }
       </div>
     </div>
